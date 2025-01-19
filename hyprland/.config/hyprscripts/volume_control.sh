bee7081ca4f298fbb4e5297948be4958bfba8f8e
Frank Stettner <frank-stettner@gmx.net>
2025-01-19 20:08:53 +0100

    hyprland: Improve script for volume control

--- hyprland/.config/hyprscripts/volume_control.sh
+++ hyprland/.config/hyprscripts/volume_control.sh
@@ -1,27 +1,46 @@
 #!/usr/bin/env bash
 
+notify_volume() {
+    notify-send \
+        -t 800 \
+        -h string:synchronous:volume-control \
+        -i ${1} \
+        -h int:value:${4} \
+        "${2}" \
+        "${3}"
+}
+
+notify_mute() {
+    notify-send \
+        -t 800 \
+        -h string:synchronous:volume-control \
+        -i ${1} \
+        "${2}" \
+        "${3}"
+}
+
 case $1 in
     up)
         pamixer -u
         pamixer -i 5
         volume="$(pamixer --get-volume)"
-        dunstify -t 1000 -r 2344 -h int:value:${volume} -i audio-volume-high "VOLUME" "Volume increased to ${volume}%" 
+        notify_volume audio-volume-high "VOLUME" "Volume increased to ${volume}%" ${volume} 
         #canberra-gtk-play -i audio-volume-change -d "changevolume"
         ;;
     down)
         pamixer -u
         pamixer -d 5
         volume="$(pamixer --get-volume)"
-        dunstify -t 1000 -r 2344 -h int:value:${volume} -i audio-volume-low "VOLUME" "Volume decreased to ${volume}%"
+        notify_volume audio-volume-low "VOLUME" "Volume decrease to ${volume}%" ${volume} 
         #canberra-gtk-play -i audio-volume-change -d "changevolume"
         ;;
     mute)
         pamixer -t
         muted="$(pamixer --get-mute)"
         if $muted ; then
-            dunstify -t 1000 -r 2344 -i audio-volume-muted "VOLUME" "Muted"
+            notify_mute audio-volume-muted "VOLUME" "Muted" 
         else
-            dunstify -t 1000 -r 2344 -i audio-volume-high "VOLUME" "Not muted"
+            notify_mute audio-volume-high "VOLUME" "Not muted" 
             #canberra-gtk-play -i audio-volume-change
         fi
         ;;
@@ -29,13 +48,11 @@ case $1 in
         pamixer --default-source -t
         muted="$(pamixer --default-source --get-mute)"
         if $muted ; then
-            dunstify -t 1000 -r 2344 -i microphone-sensitivity-muted "MICROPHONE" "Muted"
+            notify_mute microphone-sensitivity-muted "MICROPHONE" "Muted" 
         else
-            dunstify -t 1000 -r 2344 -i microphone-sensitivity-high "MICROPHONE" "Not muted"
+            notify_mute microphone-sensitivity-high "MICROPHONE" "Not muted" 
             #canberra-gtk-play -i audio-volume-change
         fi
-t
-        #dunstify ...
         ;;
 esac
 
