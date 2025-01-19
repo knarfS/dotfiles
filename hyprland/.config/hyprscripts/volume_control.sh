bcff87d234b48ca33726cdde2a13f4e0d5cab6b7
Frank Stettner <frank-stettner@gmx.net>
2025-01-19 11:39:52 +0100

    hyprland: Improve volume control script

--- hyprland/.config/hyprscripts/volume_control.sh
+++ hyprland/.config/hyprscripts/volume_control.sh
@@ -12,14 +12,14 @@ case $1 in
         pamixer -u
         pamixer -d 5
         volume="$(pamixer --get-volume)"
-        dunstify -t 1000 -r 2344 -h int:value:${volume} -i audio-volume-lower "VOLUME" "Volume decreased to ${volume}%"
+        dunstify -t 1000 -r 2344 -h int:value:${volume} -i audio-volume-low "VOLUME" "Volume decreased to ${volume}%"
         #canberra-gtk-play -i audio-volume-change -d "changevolume"
         ;;
     mute)
         pamixer -t
         muted="$(pamixer --get-mute)"
         if $muted ; then
-            dunstify -t 1000 -r 2344 -i audio-volume-muted-blocking "VOLUME" "Muted"
+            dunstify -t 1000 -r 2344 -i audio-volume-muted "VOLUME" "Muted"
         else
             dunstify -t 1000 -r 2344 -i audio-volume-high "VOLUME" "Not muted"
             #canberra-gtk-play -i audio-volume-change
@@ -27,6 +27,14 @@ case $1 in
         ;;
     mute-mic)
         pamixer --default-source -t
+        muted="$(pamixer --default-source --get-mute)"
+        if $muted ; then
+            dunstify -t 1000 -r 2344 -i microphone-sensitivity-muted "MICROPHONE" "Muted"
+        else
+            dunstify -t 1000 -r 2344 -i microphone-sensitivity-high "MICROPHONE" "Not muted"
+            #canberra-gtk-play -i audio-volume-change
+        fi
+t
         #dunstify ...
         ;;
 esac
