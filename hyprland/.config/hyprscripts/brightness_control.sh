ba52ab782a5c0ca34fd14cf848513362544a83ef
Frank Stettner <frank-stettner@gmx.net>
2025-01-21 20:12:07 +0100

    hyprland: Add script for brightness control

---
+++ hyprland/.config/hyprscripts/brightness_control.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -x
+
+# https://delightlylinux.wordpress.com/2020/10/25/bash-show-notifications-from-scripts-using-notify-send/
+# https://wiki.archlinux.org/title/Desktop_notifications
+
+notify_brightness() {
+    notify-send \
+        --app-name="brigthness-control" \
+        --expire-time=800 \
+        --transient \
+        --hint=string:synchronous:brigthness-control \
+        --icon=${1} \
+        --hint=int:value:${4} \
+        "${2}" \
+        "${3}"
+}
+
+case $1 in
+    up)
+        brightnessctl s 5%+
+        brightness="$(brightnessctl g)"
+        brightness_max="$(brightnessctl m)"
+        brigthness_percent=$((brightness/brightness_max*100))
+        notify_brightness audio-volume-high "BRIGHTNESS" "Brightness increased to ${brigthness_percent}%" ${brigthness_percent}
+        #canberra-gtk-play -i audio-volume-change -d "changevolume"
+        ;;
+    down)
+        brightnessctl s 5%-
+        brightness="$(brightnessctl g)"
+        brightness_max="$(brightnessctl m)"
+        brigthness_percent=$((brightness/brightness_max*100))
+        notify_brightness audio-volume-low "BRIGHTNESS" "Brightness decrease to ${brigthness_percent}%" ${brigthness_percent}
+        #canberra-gtk-play -i audio-volume-change -d "changevolume"
+        ;;
+esac
+
