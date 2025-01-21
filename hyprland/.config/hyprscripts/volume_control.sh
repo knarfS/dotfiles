874b3bcd9b7e56324fee68c271c4f2e7203168cb
Frank Stettner <frank-stettner@gmx.net>
2025-01-21 20:10:39 +0100

    hyprland: Improve script for volume control

--- hyprland/.config/hyprscripts/volume_control.sh
+++ hyprland/.config/hyprscripts/volume_control.sh
@@ -2,19 +2,23 @@
 
 notify_volume() {
     notify-send \
-        -t 800 \
-        -h string:synchronous:volume-control \
-        -i ${1} \
-        -h int:value:${4} \
+        --app-name="volume-control" \
+        --expire-time=800 \
+        --transient \
+        --hint=string:synchronous:volume-control \
+        --icon=${1} \
+        --hint=int:value:${4} \
         "${2}" \
         "${3}"
 }
 
 notify_mute() {
     notify-send \
-        -t 800 \
-        -h string:synchronous:volume-control \
-        -i ${1} \
+        --app-name="volume-control" \
+        --expire-time=800 \
+        --transient \
+        --hint=string:synchronous:volume-control \
+        --icon=${1} \
         "${2}" \
         "${3}"
 }
@@ -24,23 +28,23 @@ case $1 in
         pamixer -u
         pamixer -i 5
         volume="$(pamixer --get-volume)"
-        notify_volume audio-volume-high "VOLUME" "Volume increased to ${volume}%" ${volume} 
+        notify_volume audio-volume-high "VOLUME" "Volume increased to ${volume}%" ${volume}
         #canberra-gtk-play -i audio-volume-change -d "changevolume"
         ;;
     down)
         pamixer -u
         pamixer -d 5
         volume="$(pamixer --get-volume)"
-        notify_volume audio-volume-low "VOLUME" "Volume decrease to ${volume}%" ${volume} 
+        notify_volume audio-volume-low "VOLUME" "Volume decrease to ${volume}%" ${volume}
         #canberra-gtk-play -i audio-volume-change -d "changevolume"
         ;;
     mute)
         pamixer -t
         muted="$(pamixer --get-mute)"
         if $muted ; then
-            notify_mute audio-volume-muted "VOLUME" "Muted" 
+            notify_mute audio-volume-muted "VOLUME" "Muted"
         else
-            notify_mute audio-volume-high "VOLUME" "Not muted" 
+            notify_mute audio-volume-high "VOLUME" "Not muted"
             #canberra-gtk-play -i audio-volume-change
         fi
         ;;
@@ -48,9 +52,9 @@ case $1 in
         pamixer --default-source -t
         muted="$(pamixer --default-source --get-mute)"
         if $muted ; then
-            notify_mute microphone-sensitivity-muted "MICROPHONE" "Muted" 
+            notify_mute microphone-sensitivity-muted "MICROPHONE" "Muted"
         else
-            notify_mute microphone-sensitivity-high "MICROPHONE" "Not muted" 
+            notify_mute microphone-sensitivity-high "MICROPHONE" "Not muted"
             #canberra-gtk-play -i audio-volume-change
         fi
         ;;
